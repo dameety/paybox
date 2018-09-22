55327051e6939a8fdc693756f08cfaca5e7deb87
Dammy <djdamilola01@gmail.com>
2018-09-22 22:54:41 +0100

    remove comments

--- routes/web.php
+++ routes/web.php
@@ -3,31 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix(config('paybox.uri'))->group(function () {
+    Route::get('/', 'DashboardController@index')->name('paybox.dashboard');
 
-    Route::get('/', 'DashboardController@index')
-        ->name('paybox.dashboard');
-
-    //Plan
     Route::get('/plans', 'PlansController@index')->name('plan.index');
-    Route::get('/plan/create', 'PlansController@create')
-        ->name('plan.create');
-    Route::post('/plan/store', 'PlansController@store')
-        ->name('plan.store');
+    Route::get('/plan/create', 'PlansController@create')->name('plan.create');
+    Route::post('/plan/store', 'PlansController@store')->name('plan.store');
 
-    //Plan Subscriptons
-    Route::get('/plan/{slug}/subscriptions', 'PlanSubscriptionsController@index')
-        ->name('plan.subscriptions');
+    Route::get('/plan/{slug}/subscriptions', 'PlanSubscriptionsController@index')->name('plan.subscriptions');
 
-    //Subscriptions
-    Route::get('/subscriptions', 'SubscriptionsController@index')
-    ->name('subscriptions.index');
-    Route::get('/subscriptions/{id}/show', 'SubscriptionsController@show')
-    ->name('subscriptions.show');
+    Route::get('/subscriptions', 'SubscriptionsController@index')->name('subscriptions.index');
+    Route::get('/subscriptions/{id}/show', 'SubscriptionsController@show')->name('subscriptions.show');
 
-    //Erors
     Route::get('/errors', 'ErrorsController@index')->name('errors.index');
 
-    //creating a new subscription
     Route::post('/user-subscription/store', 'UserSubscriptionsController@store');
 });
 
@@ -36,32 +24,25 @@ Route::group([
     'namespace' => 'Ajax',
 ], function () {
 
-    //Payment info
     Route::post('/payment-info/update', 'PaymentInfoController@update');
     Route::get('/payment-info/show', 'PaymentInfoController@show');
 
-    //User Subscriptions
     Route::get('/user-subscription/{planName}/show', 'UserSubscriptionsController@show');
     Route::post('/user-subscription/store', 'UserSubscriptionsController@store');
     Route::patch('/user-subscription/{plan}/update', 'UserSubscriptionsController@update');
 
-    //Cancelled subscriptions
     Route::post('/cancelled-subscription/store', 'CancelledUserSubscriptionsController@store');
     Route::get('/cancelled-subscription/show', 'CancelledUserSubscriptionsController@show');
     Route::delete('/cancelled-subscription/destroy', 'CancelledUserSubscriptionsController@destroy');
 
-    //Create a sub from resumeandswap
     Route::post('/subscription/store', 'SubscriptionsController@store');
 
-    //Plan
     Route::delete('/plan/{slug}/delete', 'PlansController@destroy');
     Route::get('/plans', 'PlansController@index');
 
-    //Errors
     Route::delete('/error/{slug}/delete', 'ErrorsController@destroy');
 
-    //Invoices
     Route::get('/invoice/{id}/show', 'InvoicesController@show');
-    //get authenticated user's invoices
+
     Route::get('/invoices/show', 'UserInvoicesController@show');
 });
